53872301142ba72c0e0a60868582ad9ae1957ab5
Soheil Movahedan <soheil.movahhedan@gmail.com>
2022-01-03 17:29:53 +0330

    Customize Error type in apps/website accoarding to lib/fetcher

--- apps/website/index.d.ts
+++ apps/website/index.d.ts
@@ -1,6 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import 'node';
+
 declare module '*.svg' {
   const content: any;
   export const ReactComponent: any;
   export default content;
+}
+
+declare global {
+  interface Error extends Error {
+    apiError?: Response;
+  }
 }
