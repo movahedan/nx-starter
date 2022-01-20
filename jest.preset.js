0d8dd5383214681a274d6db045dd398370290598
Soheil Movahedan <soheil.movahhedan@gmail.com>
2022-01-20 10:04:57 +0330

    Fix project materials

--- jest.preset.js
+++ jest.preset.js
@@ -3,4 +3,5 @@ const nxPreset = require('@nrwl/jest/preset');
 module.exports = {
   ...nxPreset,
   testMatch: ['**/+(*.)+(test).+(ts)?(x)'],
+  clearMocks: true,
 };
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,27 +1,7 @@
-const micromatch = require('micromatch');
-const path = require('path');
-
 module.exports = {
-  '*.{json,html,css,js,jsx,ts,tsx}': (files) => {
-    // https://github.com/okonet/lint-staged#example-ignore-files-from-match
-    const cwd = process.cwd();
-    const relativeFiles = files.map((file) => path.relative(cwd, file));
-    const notMinifiedFiles = micromatch.not(relativeFiles, ['*.min.*'], {
-      matchBase: true,
-    });
-
-    return [`yarn nx format:write --files=${notMinifiedFiles.join(',')}`];
-  },
-  '*.{js,jsx,ts,tsx}': (files) => {
-    const cwd = process.cwd();
-    const relativeFiles = files.map((file) => path.relative(cwd, file));
-
-    return [
-      'yarn nx affected --target=type-check',
-      `yarn nx affected:lint --fix --parallel --files=${relativeFiles.join(
-        ','
-      )}`,
-      `yarn nx affected:test --files=${relativeFiles.join(',')}`,
-    ];
-  },
+  '*.{js,jsx,ts,tsx}': () => [
+    'yarn nx affected --target=type-check',
+    'yarn nx affected:lint --fix --parallel',
+    'yarn nx affected:test',
+  ],
 };
