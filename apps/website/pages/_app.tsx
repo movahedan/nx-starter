2af9ecbfd9b070421969dd0be2a3fb7443c4ac26
Soheil Movahedan <soheil.movahhedan@gmail.com>
2022-04-16 00:32:32 +0200

    Add media query example in website

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -2,18 +2,20 @@ import Head from 'next/head';
 
 import '../pages.styles/main.css';
 
+import { MediaQueriesProvider } from '@root/modules-media-queries';
+
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <MediaQueriesProvider initialData={pageProps}>
       <Head>
         <title>Welcome to website!</title>
       </Head>
       <main className='app'>
         <Component {...pageProps} />
       </main>
-    </>
+    </MediaQueriesProvider>
   );
 };
 
