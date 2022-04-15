b30edb0d53ffb2eebea5e0eb4ca6729ac14085af
Soheil Movahedan <soheil.movahhedan@gmail.com>
2022-04-16 00:33:12 +0200

    Add test converage threshold for the wole project

--- jest.preset.js
+++ jest.preset.js
@@ -4,4 +4,16 @@ module.exports = {
   ...nxPreset,
   testMatch: ['**/+(*.)+(test).+(ts)?(x)'],
   clearMocks: true,
+  restoreMocks: true,
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  coverageReporters: ['html', 'text', 'text-summary'],
+  coverageThreshold: {
+		global: {
+			statement: 80,
+			branches: 80,
+			functions: 80,
+			lines: 80,
+		},
+	},
 };
