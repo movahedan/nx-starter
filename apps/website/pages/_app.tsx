ef410621dafd3da6f51dc4de37e4985a473fa712
Soheil Movahedan <soheil.movahhedan@gmail.com>
2022-05-23 04:14:18 +0200

    refactor(website,ui): Use new ComposeProvider from utilities-react instead of ui
    ComposeProvider has been moved to utilities-react
    
    BREAKING CHANGE: You cannot import ComposeProvider from ui/utils anymore

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -4,14 +4,10 @@ import Head from 'next/head';
 import { Fragment } from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import {
-  theme,
-  GlobalStyle,
-  ComposeProviders,
-  fontLinksProps,
-} from '@nx-starter/ui';
+import { theme, GlobalStyle, fontLinksProps } from '@nx-starter/ui';
 
 import { MediaQueriesProvider } from '@nx-starter/modules/media-queries';
+import { ComposeProviders } from '@nx-starter/utilities-react';
 
 import type { AppWithLayoutProps } from 'next/app';
 
