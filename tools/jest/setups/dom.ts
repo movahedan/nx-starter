cba63d2158fc20b3941aa7f9af281398d8f81844
Soheil Movahedan <soheil.movahhedan@gmail.com>
2022-05-23 04:09:33 +0200

    feat(root): Add navigator mock to jest/setups/dom.ts

--- tools/jest/setups/dom.ts
+++ tools/jest/setups/dom.ts
@@ -1,1 +1,8 @@
 require('@testing-library/jest-dom');
+
+// @ts-expect-error mocking readonly variable geoLocation
+global.window.navigator.geolocation = {
+  clearWatch: jest.fn(),
+  getCurrentPosition: jest.fn(),
+  watchPosition: jest.fn(),
+};
