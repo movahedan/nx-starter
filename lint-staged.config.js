1023f51b5fbd2cef98c87b78a4b1201491959355
Soheil Movahedan <soheil.movahhedan@gmail.com>
2022-05-23 04:19:13 +0200

    chore(root): Make lint-staged faster by removing untracked files from linting process

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,9 +3,9 @@ const micromatch = require('micromatch');
 module.exports = {
   '*.{ts,tsx}': () => ['yarn nx affected --target=type-check'],
   '*.{js,ts,tsx}': () => [
-    'yarn nx affected:lint --fix',
-    'yarn nx affected:test --color',
-    'yarn nx affected:build',
+    'yarn nx affected:lint --untracked=false --fix',
+    'yarn nx affected:test --untracked=false --color',
+    'yarn nx affected:build --untracked=false',
   ],
   '*.{json,html,css,scss,less,sass,svg}': (files) => {
     // https://github.com/okonet/lint-staged#example-ignore-files-from-match
